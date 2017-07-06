f19bbc060161b496e25c7232f0d337ea915522a4
Magnus Jerre <magnus.jerre@gmail.com>
2017-07-06 20:13:49 +0200

    Implementing shooting and destruction of targets.
    The destruction is visible for both the VR player and the NVR players.

--- Assets/Scripts/PlayerScripts/PlayerVR.cs
+++ Assets/Scripts/PlayerScripts/PlayerVR.cs
@@ -9,29 +9,51 @@ public class PlayerVR : NetworkBehaviour {
 	private LineRenderer lineRenderer;
 	private PlayerHand playerHand;
 
+	private LayerMask shootingMask;
+
+	private Target[] allTargets;
+
 	void Start () {
 		lineRenderer = GetComponent<LineRenderer>();
 		lineRenderer.enabled = false;
 
 		// GameObject.FindObjectOfType<PlayerHand>().SetPlayerVR(this);
 		playerHand = GameObject.FindObjectOfType<PlayerHand>();
+		shootingMask = LayerMask.NameToLayer("Shootable");
+		allTargets = GameObject.FindObjectsOfType<Target>();
 	}
 
 	void Update() {
 		if (!hasAuthority) {
 			return;
 		}
-
+		Debug.DrawRay(playerHand.Muzzle().position, playerHand.Muzzle().position + playerHand.Muzzle().forward * 10);
 		if (playerHand.ReleasedTrigger()) {
-			CmdFire(playerHand.Muzzle().position, playerHand.Muzzle().forward * 10);
+			CmdFire(playerHand.Muzzle().position, playerHand.Muzzle().position + playerHand.Muzzle().forward * 10);
 		}
 	}
 
 	[Command]
-	public void CmdFire(Vector3 start, Vector3 end) {
-		//Do some checks for hit info
-		RpcShowFire(start, end);
-	}
+    public void CmdFire(Vector3 start, Vector3 end)
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(start, end - start, out hitInfo, 10f, ~shootingMask.value))
+        {
+			Debug.Log("Raycasthit, woho");
+            Target hitTarget = hitInfo.collider.GetComponentInParent<Target>();
+            if (hitTarget != null)
+            {
+                Vector3 offset = hitInfo.point - hitTarget.Center.position;
+                RpcShowHitFire(start, hitTarget.id, offset);
+            }
+        }
+        else
+        {
+            RpcShowFire(start, end);
+        }
+
+		Debug.Log("hitInfo.collider: " + hitInfo.collider);
+    }
 
 	[ClientRpc]
 	public void RpcShowFire(Vector3 start, Vector3 end) {
@@ -39,5 +61,26 @@ public class PlayerVR : NetworkBehaviour {
 			start, end
 		});
 		lineRenderer.enabled = true;
+		Debug.Log("RpcShowFire");
+	}
+
+	[ClientRpc]
+	public void RpcShowHitFire(Vector3 start, int targetId, Vector3 centerOffset) {
+		Debug.Log("RpcShowHitFire");
+		Target target = GetTargetById(targetId);
+		lineRenderer.SetPositions(new Vector3[]{
+			start, target.Center.position + centerOffset
+		});
+		lineRenderer.enabled = true;
+		target.RegisterHit();
+	}
+
+	private Target GetTargetById(int id) {
+		foreach (Target target in allTargets) {
+			if (target.id == id) {
+				return target;
+			}
+		}
+		return null;
 	}
 }
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+
+	[SerializeField]
+	private Transform center;
+	public Transform Center { get { return center; } }
+
+	public int id;
+
+	public bool IsUp;
+
+	private ParticleSystem ps;
+	[SerializeField]
+	private GameObject targetModel;
+
+	// Use this for initialization
+	void Start () {
+		ps = GetComponentInChildren<ParticleSystem>();
+		ShowTarget();
+	}
+
+	public void RegisterHit() {
+		IsUp = false;
+		targetModel.SetActive(false);
+		ps.Play();
+	}
+
+	public void ShowTarget() {
+		IsUp = true;
+		targetModel.SetActive(true);
+		ps.Stop();
+	}
+}
