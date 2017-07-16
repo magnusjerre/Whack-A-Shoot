0581a0da90ae57455bad0c749aadd6d0d79a64a0
Magnus Jerre <magnus.jerre@gmail.com>
2017-07-16 21:07:34 +0200

    Creating a 360 degrees rotating target.

---
+++ Assets/Scripts/Transformers/Rotating.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour 
+{
+	public Axis localAxis;
+	public float totalTime;
+	
+	void Update () 
+	{
+		float dr = (360f / totalTime) * Time.deltaTime;
+		Space space = Space.World;
+		if (localAxis == Axis.FORWARD) 
+		{
+			transform.Rotate (transform.forward, dr, space);
+		}
+		else if (localAxis == Axis.RIGHT) 
+		{
+			transform.Rotate (transform.right, dr, space);
+		}
+		else 
+		{
+			transform.Rotate(transform.up, dr, space);
+		}
+	}
+}
